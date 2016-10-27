6f352351b1c462cbc6f3de9c54e5f383f4f8451e
shinelon <syq_sunshine@163.com>
2016-10-27 16:12:01 +0800

    init particles

--- assets/js/app.js
+++ assets/js/app.js
@@ -26,12 +26,70 @@
           $('.tab-pane').length && $($('.cat-tag-menu li a')[0]).tab('show');
       }
     }
+    /**
+     * 
+     * */
+    function particlesFunc(){
+    	/* default dom id (particles-js) */
+    	//particlesJS();
 
+    	/* config dom id */
+    	//particlesJS('dom-id');
+
+    	/* config dom id (optional) + config particles params */
+    	particlesJS('particles-js', {
+    	  particles: {
+    	    color: '#fff',
+    	    shape: 'circle', // "circle", "edge" or "triangle"
+    	    opacity: 1,
+    	    size: 4,
+    	    size_random: true,
+    	    nb: 150,
+    	    line_linked: {
+    	      enable_auto: true,
+    	      distance: 100,
+    	      color: '#fff',
+    	      opacity: 1,
+    	      width: 1,
+    	      condensed_mode: {
+    	        enable: false,
+    	        rotateX: 600,
+    	        rotateY: 600
+    	      }
+    	    },
+    	    anim: {
+    	      enable: true,
+    	      speed: 1
+    	    }
+    	  },
+    	  interactivity: {
+    	    enable: true,
+    	    mouse: {
+    	      distance: 300
+    	    },
+    	    detect_on: 'canvas', // "canvas" or "window"
+    	    mode: 'grab',
+    	    line_linked: {
+    	      opacity: .5
+    	    },
+    	    events: {
+    	      onclick: {
+    	        enable: true,
+    	        mode: 'push', // "push" or "remove"
+    	        nb: 4
+    	      }
+    	    }
+    	  },
+    	  /* Retina Display Support */
+    	  retina_detect: true
+    	});
+    }
     // watch hash change and activate relevant tab
     $(window).on('hashchange', activateTab);
 
     // initial activation
     activateTab();
+    particlesFunc();
   };
 
   // run init on document ready
